e948dbbd19547332bb2e1a3ad220206d2a712f41
Brandon Miller <BrandonMiller355@gmail.com>
2015-11-19 13:50:48 -0500

    Spaced and labeled buttons; Added onClick action; started toast handling

--- app/src/main/java/com/example/android/portfolio/MainActivity.java
+++ app/src/main/java/com/example/android/portfolio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.portfolio;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,23 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.container, new PlaceholderFragment())
-//                    .commit();
-//        }
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//TODO:Delete this stuff
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     @Override
@@ -62,48 +50,27 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-/*    public static class PlaceholderFragment extends Fragment {
+    public void toastMessage(View view) {
+        //source here taken from: http://developer.android.com/guide/topics/ui/notifiers/toasts.html
+//        Context context = getApplicationContext()
+        String toastMessage = "Unknown app";
 
-        public PlaceholderFragment() {
+        String viewName = "";
+        viewName = new viewToString(view);
+
+        switch(viewName) {
+
+            case viewName.endsWith("spotify_streamer}"):
+
+
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            String[] myApps = {
-                    "SPOTIFY STREAMER",
-                    "SCORES APP",
-                    "LIBRARY APP",
-                    "BUILD IT BIGGER",
-                    "XYZ READER",
-                    "CAPSTONE: MY OWN APP"
-            };
+        Toast toast = Toast.makeText(getApplicationContext(), toastMessage, Toast.LENGTH_SHORT);
+        toast.show();
 
-            ArrayAdapter<String> myDataAdapter;
+        //toastMessage(view.getRootView());
+    }
 
-//            myDataAdapter = new ArrayAdapter<String>(getActivity(), //TODO: see if can change that to rootView instead, since we know it
-//                    R.layout.list_item_appname,
-//                    R.id.list_item_appname_button,
-//                    myApps
-//            );
 
-            //FrameLayout framelayout_main = (FrameLayout) findViewById(R.id.framelayout_main);
-//            ListView listview_apps = (ListView) rootView.findViewById(R.id.listview_apps);
-//
-//            listview_apps.setAdapter(myDataAdapter);
-
-            TextView textView_apps = (TextView) rootView.findViewById(R.id.textView_apps);
-            textView_apps
-
-            return rootView;
-        }
-
-//        public View onCreateView2(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View testView = inflater.inflate(R.layout.fragment_test, container, false);
-//            return testView;
-//        }
-    }*/
 }
